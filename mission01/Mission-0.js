369389aa8f9fc0e9fb42c2e403a87a437c9ebef2
leeginger <onepagecode@naver.com>
2024-11-03 17:02:44 +0900

    11월 첫째주 자바스크립트 과제

--- mission01/Mission-0.js
+++ mission01/Mission-0.js
@@ -27,13 +27,13 @@ function getValueAtObject(obj, key) {
 
 const numbers = [10, 20, 30, 40, 50];
 
-console.log(getNumberAtArray(numbers, 2)); // 30
-console.log(getNumberAtArray(numbers, 4)); // 50
-console.log(getNumberAtArray(numbers, 5)); // Error!
-console.log(getNumberAtArray(numbers, -1)); // Error!
+// console.log(getNumberAtArray(numbers, 2)); // 30
+// console.log(getNumberAtArray(numbers, 4)); // 50
+// console.log(getNumberAtArray(numbers, 5)); // Error!
+// console.log(getNumberAtArray(numbers, -1)); // Error!
 
 function getNumberAtArray(arr, index) {
-  if (arr in person) {
+  if (arr in numbers) {
     return arr[index];
   } else {
     return arr[index];
